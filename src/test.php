82bc25b2ce215a99d7d4f6de9a86bd46ffa2388d
rakesh635 <rakesh635@gmail.com>
2016-06-25 15:38:22 +0530

    Update test.php

--- src/test.php
+++ src/test.php
@@ -12,13 +12,15 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT id, firstname FROM test";
+//$sql = "SELECT id, firstname FROM test";
+$sql = "Select comment_ID,comment_author,comment_content from wp_comments";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"];
+        //echo "id: " . $row["id"]. " - Name: " . $row["firstname"];
+        echo "id: " . $row["comment_ID"]. " ; Author: " . $row["comment_author"]. " ; Comment: " . $row["comment_content"];
     }
 } else {
 	echo "No row";
